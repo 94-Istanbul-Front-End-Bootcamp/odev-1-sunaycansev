941e9c764caa5077016e94189606d8897b20cdf8
sunaycansev <fsunaycansev@gmail.com>
2021-06-21 20:03:19 +0300

    update button function

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const age = document.querySelector("#age");
 const linkednLink = document.querySelector("#linkedinUrl");
 const githubLink = document.querySelector("#githubUrl");
 
+// setting name surname and age variables
+const NAME = "Ferhat Sunay";
+const SURNAME = "Cansev";
+const AGE = 25;
+
 // setting attributes to <a> elements
 linkednLink.setAttribute("href", "https://www.linkedin.com/in/fsunaycansev/");
 githubLink.setAttribute("href", "https://github.com/sunaycansev");
 
 //getfullName function
 function getfullName() {
-  fullName.innerHTML = "Ferhat Sunay Cansev";
-  age.innerHTML = "24";
+  fullName.innerHTML = `${NAME} ${SURNAME}`;
+  age.innerHTML = AGE;
   btn.style.display = "none";
 }
